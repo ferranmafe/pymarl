135319cb3ac110a2df92040d25f2995766108ad7
Ferran Martínez <ferran.mafe@gmail.com>
2021-05-02 11:05:52 +0200

    Modified single RNN outputs to increase performance

--- src/modules/agents/rnn_agent.py
+++ src/modules/agents/rnn_agent.py
@@ -35,7 +35,7 @@ class RNNAgent(nn.Module):
             h = self.rnn(x, h_in)
             q1 = self.fc21(h)
             q2 = self.fc22(h)
-            return self.__combine_torch_tensors(q1, q2), h
+            return torch.stack((q1, q2), dim=2).view(q1.size()[0] * 2, q1.size()[1]), h
         elif self.args.agent_type == '2_units_combined_output':
             x = F.relu(self.fc1(inputs.view(-1, self.input_shape * 2)))
             h_in = hidden_state.reshape(-1, self.args.rnn_hidden_dim * 2)
@@ -50,18 +50,9 @@ class RNNAgent(nn.Module):
             return q, h
 
     @staticmethod
-    def __combine_torch_tensors(tensor1, tensor2):
-        output = torch.empty([tensor1.size()[0] * 2, tensor1.size()[1]])
-        for i in range(tensor1.size()[0]):
-            output[i * 2][:] = tensor1[i][:]
-            output[i * 2 + 1][:] = tensor2[i][:]
-        return output
-
-    @staticmethod
     def __decode_combined_output(tensor):
-        output = torch.empty(tensor.size()[0] * 2, int(math.sqrt(tensor.size()[1])))
-        for i in range(tensor.size()[0]):
-            for j in range(int(math.sqrt(tensor.size()[1]))):
-                output[i * 2][j] = max(tensor[i][k] for k in range(tensor.size()[1]) if j <= k < j + int(math.sqrt(tensor.size()[1])))
-                output[i * 2 + 1][j] = max(tensor[i][k - 1] for k in range(j, tensor.size()[1], int(math.sqrt(tensor.size()[1]))))
-        return output
+        left_units = torch.max(tensor.view(3, 30, 30), dim=2).values
+        right_units = torch.max(tensor.view(3, 30, 30), dim=1).values
+        return torch.stack((left_units, right_units), dim=2)\
+            .view(tensor.size()[0] * 2, int(math.sqrt(tensor.size()[1])))
+
