de13b746a49aa6263c3751ad4b39ba150ca25491
Ferran Martínez <ferran.mafe@gmail.com>
2021-05-18 17:25:14 +0200

    Added code to 1 pair

--- src/modules/agents/rnn_agent.py
+++ src/modules/agents/rnn_agent.py
@@ -16,9 +16,14 @@ class RNNAgent(nn.Module):
             self.fc21 = nn.Linear(args.rnn_hidden_dim * 2, args.n_actions)
             self.fc22 = nn.Linear(args.rnn_hidden_dim * 2, args.n_actions)
         elif self.args.agent_type == '2_units_combined_output':
-            self.fc1 = nn.Linear(input_shape * 2, args.rnn_hidden_dim * 2)
-            self.rnn = nn.GRUCell(args.rnn_hidden_dim * 2, args.rnn_hidden_dim * 2)
-            self.fc2 = nn.Linear(args.rnn_hidden_dim * 2, args.n_actions ** 2)
+            # Pair of units
+            self.fc11 = nn.Linear(input_shape, args.rnn_hidden_dim)
+            self.rnn1 = nn.GRUCell(args.rnn_hidden_dim, args.rnn_hidden_dim)
+            self.fc12 = nn.Linear(args.rnn_hidden_dim, args.n_actions)
+            # Single unit
+            self.fc21 = nn.Linear(input_shape * 2, args.rnn_hidden_dim * 2)
+            self.rnn2 = nn.GRUCell(args.rnn_hidden_dim * 2, args.rnn_hidden_dim * 2)
+            self.fc22 = nn.Linear(args.rnn_hidden_dim * 2, args.n_actions ** 2)
         else:
             self.fc1 = nn.Linear(input_shape, args.rnn_hidden_dim)
             self.rnn = nn.GRUCell(args.rnn_hidden_dim, args.rnn_hidden_dim)
@@ -26,7 +31,8 @@ class RNNAgent(nn.Module):
 
     def init_hidden(self):
         # make hidden states on same device as model
-        return self.fc1.weight.new(1, self.args.rnn_hidden_dim).zero_()
+        return self.fc11.weight.new(1, self.args.rnn_hidden_dim).zero_(), \
+               self.fc21.weight.new(1, self.args.rnn_hidden_dim * 2).zero_()
 
     def forward(self, inputs, hidden_state):
         if self.args.agent_type == '2_units':
@@ -37,11 +43,24 @@ class RNNAgent(nn.Module):
             q2 = self.fc22(h)
             return torch.stack((q1, q2), dim=2).view(q1.size()[0] * 2, q1.size()[1]), h
         elif self.args.agent_type == '2_units_combined_output':
-            x = F.relu(self.fc1(inputs.view(-1, self.input_shape * 2)))
-            h_in = hidden_state.reshape(-1, self.args.rnn_hidden_dim * 2)
-            h = self.rnn(x, h_in)
-            q = self.fc2(h)
-            return self.__decode_combined_output(q), h
+            inputs = inputs.view(self.args.n_agents, self.input_shape, -1)
+
+            individual_inputs = inputs[:self.args.n_agents - 2, :, :]
+            individual_inputs = individual_inputs.view(-1, self.input_shape)
+
+            x = F.relu(self.fc11(individual_inputs))
+            h_in = hidden_state[0].reshape(-1, self.args.rnn_hidden_dim)
+            h_ind = self.rnn1(x, h_in)
+            q_ind = self.fc12(h_ind)
+
+            pairs_inputs = inputs[self.args.n_agents - 2:, :, :]
+            pairs_inputs = pairs_inputs.view(-1, self.input_shape * 2)
+
+            x = F.relu(self.fc21(pairs_inputs))
+            h_in = hidden_state[1].reshape(-1, self.args.rnn_hidden_dim * 2)
+            h_p = self.rnn2(x, h_in)
+            q_p = self.fc22(h_p)
+            return torch.cat((q_ind, self.__decode_combined_output(q_p)), 0), (h_ind, h_p)
         else:
             x = F.relu(self.fc1(inputs))
             h_in = hidden_state.reshape(-1, self.args.rnn_hidden_dim)
